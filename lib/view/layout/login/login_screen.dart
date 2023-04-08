6ded2041522b0617b579ef40b950ada453fc2080
Ahmed khaled <93054790+Ahmed-Khaled-ibrahem@users.noreply.github.com>
2023-04-08 17:40:51 +0200

    login page

--- lib/view/layout/login/login_screen.dart
+++ lib/view/layout/login/login_screen.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../../resources/theme/app_theme.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: Colors.red,);
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        backgroundColor: darkMainColor,
+        appBar: AppBar(
+          backgroundColor: darkMainColor,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Lottie.asset(
+                'assets/lottie/coffee-time2.zip',
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: TextField(
+                  // cursorColor: Theme.of(context).cursorColor,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.person_pin_rounded),
+                    labelText: 'Name',
+                    labelStyle: TextStyle(
+                      color: Color(0xFF6200EE),
+                    ),
+                    helperText: 'your name to be sent with the orders',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20,),
+              ElevatedButton(
+                  onPressed: (){},
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(const Size(250, 30)),
+                      elevation: MaterialStateProperty.all(7)),
+                  child: const Text("Login")),
+              TextButton(onPressed: (){}, child: const Text("Are you Admin?"))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/view/resources/theme/app_theme.dart
+++ lib/view/resources/theme/app_theme.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+Color mainColor = const Color(0xFFC8B6A6);
+Color darkMainColor = const Color(0xFFA9907E);
+
 ThemeData appTheme = ThemeData(
 primarySwatch: Colors.blue,
   useMaterial3: true
